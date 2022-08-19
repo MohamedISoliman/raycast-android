b3dd4a109f5cb5cbbc9dd1972956f85c38c4255c
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2022-08-19 17:02:10 +0200

    show directories list in a folder

--- src/list-android.tsx
+++ src/list-android.tsx
@@ -1,17 +1,54 @@
-import { ActionPanel, Detail, List, Action } from "@raycast/api";
+import { ActionPanel, Detail, List, Action, PreferenceValues, showToast, Toast, popToRoot } from "@raycast/api";
+import { getPreferenceValues } from "@raycast/api";
+import { useState, useEffect } from "react";
+import { PathLike, readdir } from "fs";
+
 
 export default function Command() {
+
+  const [items, setItems] = useState<string[]>(() => [])
+  const [loading, setLoading] = useState(true)
+
+  const preferences: PreferenceValues = getPreferenceValues<PreferenceValues>();
+  const projectsDirectory = preferences.ANDROID_DIRECTORY;
+
+  delay(40000)
+
+  getDirectories(projectsDirectory, (files: string[]) => {
+    setItems(files)
+    setLoading(false)
+
+  }, function (err: NodeJS.ErrnoException) {
+    showToast(Toast.Style.Failure, "Something wrong happed!", err.message)
+    setLoading(false)
+  });
+
   return (
-    <List>
-      <List.Item
-        icon="list-icon.png"
-        title="Greeting"
-        actions={
-          <ActionPanel>
-            <Action.Push title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
-          </ActionPanel>
-        }
-      />
-    </List>
-  );
+    <List isLoading={loading}>
+      {
+        items?.map((project, index) => (
+          <List.Item key={index} title={project} />
+        ))
+      }
+    </List>);
+}
+
+
+const getDirectories = (
+  source: PathLike,
+  onSuccess: (directories: string[]) => string[],
+  onError: (err: NodeJS.ErrnoException) => void
+) =>
+  readdir(source, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      onError(err);
+    } else {
+      const directories = files.filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name);
+      onSuccess(directories);
+    }
+  });
+
+
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
